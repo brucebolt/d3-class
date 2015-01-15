e7d4c9007b8a3af1e9d422c0be35b4555c6c8bf7
Bruce Bolt <bbolt@ebi.ac.uk>
2015-01-15 09:59:24 +0000

    first exercise

--- exercises/selections/viz.js
+++ exercises/selections/viz.js
@@ -1,6 +1,13 @@
 // TODO select all circle elements and change their stroke style to red
+
+d3.selectAll('circle').style('stroke', 'red');
+
 // TODO select second circle and change it's r attribute to 500
+
+d3.selectAll('circle:nth-child(2)').attr('r', 500);
+
 // TODO select third circle and change its cx attribute to 650
 
+d3.selectAll('circle:nth-child(3)').attr('cx', 650);
 
 // TODO up to you :)
