4160399aec5c6cd1678d9d749517cff05eae26e6
Bruce Bolt <bbolt@ebi.ac.uk>
2015-01-15 11:59:29 +0000

    events exercise

--- exercises/events/viz.js
+++ exercises/events/viz.js
@@ -6,16 +6,25 @@ var data = [
   ];
 
 // TODO select circles, bind data
-var circle;
+var circle = d3
+  .selectAll('circle')
+  .data(data);
   // TODO make 'onmove' handler for for `mousemove` event
+circle
+  .on('mousemove', onmove);
   // TODO make 'onout' handler for for `mouseout` event
-
+circle
+  .on('mouseout', onout);
 
 function onmove(d, i) {
   // TODO change the fill of current element
+  d3.select(this)
+    .style('fill', d.color);
 }
 
 function onout(d, i) {
   // TODO reset fill to original (style null)
+  d3.select(this)
+    .style('fill', null);
 }
 
